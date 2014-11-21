d05059fb162d3026284bee7a82b925a0d5adaa19
matteo88 <kingletti88@gmail.com>
2014-11-21 17:54:09 +0100

    Integrata scrittura su cd /dvd. Da integrare whiptail (fero).

--- scrivi_chiavetta_ed_esegui_suono.sh
+++ scrivi_chiavetta_ed_esegui_suono.sh
@@ -11,6 +11,10 @@ if [ ! -f "$1" ];  then
     exit 101
 fi
 
+#Here choose device type with wiptail
+device_type=chosen_type
+
+
 
 iso="$1"
 device=/dev/sdb
@@ -21,8 +25,12 @@ if [ "$scelta" = "y" ] || [ "$scelta" = "Y" ]; then
     echo "Questa è l'operazione che avrei eseguito sul tuo sistema"
     echo "Se ti interessa rimuovi 'echo' davanti al comando dd"
     echo "Ma stai attento perche' se non scrivi il dispositivo giusto potrai trovarti con un sistema inusabile!"
-
-    echo sudo dd if="${iso}" of="${device}" && \
+    if [ "$device_type" = "usb" ]; then
+        echo sudo dd if="${iso}" of="${device}"
+    else if [ "$device_type" = "cd" ] || [ "$device_type" = "dvd" ]; then 
+        echo sudo wodim dev="${device}" driveropts=burnfree,noforcespeed fs=14M speed=8 -dao -eject -overburn -v "${iso}" 
+    fi
+    fi
     espeak -v it "Fatto tutto. Goditi la tua $iso" 2> /dev/null
 else
     espeak -v it "Paura eh?!?! Ricorda: meglio un giorno da leoni..." 2> /dev/null
