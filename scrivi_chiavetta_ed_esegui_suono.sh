9995a0aa9aff00d0dc0f402cf10983bb78120f29
Luca Ferroni <luca@befair.it>
2014-11-28 17:36:51 +0100

    Introdotto menu di scelta con whiptail
    Testato settando manualmente il device! 0.0.1-dev !!! :)

--- scrivi_chiavetta_ed_esegui_suono.sh
+++ scrivi_chiavetta_ed_esegui_suono.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-    echo "Utilizzo: $0 <iso>"
+    echo "Utilizzo: $0 <directory>"
     exit 100
 fi
 
-if [ ! -f "$1" ];  then
-    echo "$1 non è un file!"
-    echo "Utilizzo: $0 <iso>"
+if [ ! -d "$1" ];  then
+    echo "$1 non è una directory!"
+    echo "Utilizzo: $0 <directory>"
     exit 101
 fi
 
-#Here choose device type with wiptail
-device_type=chosen_type
 
+dirname="$1"
+iso_list=$(cd "$dirname" && ls *.iso)
 
+OPTIONS=""
+c=0;
+for iso_file in $iso_list; do 
+    c=$(($c+1))
+    OPTIONS="$OPTIONS $iso_file $c   off"
+done
 
-iso="$1"
-device=/dev/sdb
+while [ -z "$iso" ]; do
+    iso=$(whiptail --radiolist "Scegli il sistema da portarti a casa" 25 65 20 ${OPTIONS} 3>&1 1>&2 2>&3)
+done
+
+iso_absolute_path="$dirname"/"$iso"
+
+#Here choose device type with whiptail
+device_type=usb
+
+device=/dev/sdc
 
 read -p "Stai per scrivere l'immagine $iso sul device $device. Sei sicuro [y/N]? " scelta
 
@@ -26,9 +40,9 @@ if [ "$scelta" = "y" ] || [ "$scelta" = "Y" ]; then
     echo "Se ti interessa rimuovi 'echo' davanti al comando dd"
     echo "Ma stai attento perche' se non scrivi il dispositivo giusto potrai trovarti con un sistema inusabile!"
     if [ "$device_type" = "usb" ]; then
-        echo sudo dd if="${iso}" of="${device}"
+        /bin/dd if="${iso_absolute_path}" of="${device}"
     else if [ "$device_type" = "cd" ] || [ "$device_type" = "dvd" ]; then 
-        echo sudo wodim dev="${device}" driveropts=burnfree,noforcespeed fs=14M speed=8 -dao -eject -overburn -v "${iso}" 
+        echo sudo wodim dev="${device}" driveropts=burnfree,noforcespeed fs=14M speed=8 -dao -eject -overburn -v "${iso_absolute_path}" 
     fi
     fi
     espeak -v it "Fatto tutto. Goditi la tua $iso" 2> /dev/null
