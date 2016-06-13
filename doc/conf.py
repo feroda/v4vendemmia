6d3311d0985461f822d1f0ace2087a6e2bf23e8c
Radeox <dawid.weglarz95@gmail.com>
2016-06-13 21:44:24 +0200

    Aggiornata cofiguarazione LaTex per togliere le pagine bianche nel PDF

--- doc/conf.py
+++ doc/conf.py
@@ -239,21 +239,8 @@ htmlhelp_basename = 'V4Vendemmiadoc'
 # -- Options for LaTeX output ---------------------------------------------
 
 latex_elements = {
-     # The paper size ('letterpaper' or 'a4paper').
-     #
-     # 'papersize': 'letterpaper',
-
-     # The font size ('10pt', '11pt' or '12pt').
-     #
-     # 'pointsize': '10pt',
-
-     # Additional stuff for the LaTeX preamble.
-     #
-     # 'preamble': '',
-
-     # Latex figure (float) alignment
-     #
-     # 'figure_align': 'htbp',
+    'classoptions': ',openany,oneside', 
+    'babel' : '\\usepackage[italian,shorthands=off]{babel}'
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
