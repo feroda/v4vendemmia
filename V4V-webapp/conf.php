bd1ed309e6252bfa3dfe019e3882c6dd57d2960e
Radeox <dawid.weglarz95@gmail.com>
2016-07-02 23:27:46 +0200

    [web] aggiunti esempi generici

--- V4V-webapp/conf.php
+++ V4V-webapp/conf.php
@@ -1,9 +1,9 @@
 <?php
     $devices_unwritable = [ "sda", "sr0" ];
-    $ISO_PATH = "/srv/http/iso/";
+    $ISO_PATH = "example_iso/";
 
     $custom_tab = false;
 
-    $tmp_dir = "/srv/http/temp/";
+    $tmp_dir = "temp/";
     $tmp_file = "V4V";
 ?>
