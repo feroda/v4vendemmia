8df164c21bcbea273ea3f9a10c89998a3adc54a0
Radeox <dawid.weglarz95@gmail.com>
2017-09-25 17:20:30 +0200

    Rename typo name

--- v4v_bash/V4V.sh
+++ v4v_bash/V4V.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#Definizioni
-TITLE="V4Vendemmia 0.3"         #Titolo
-PASSWORD="PDP"                  #Password
+# Definizioni
+TITLE="V4Vendemmia 0.3"         # Titolo
+PASSWORD="PDP"                  # Password
 
 dirname="$1"
 
-#Controllo se è stata passata correttamente la DIR delle ISO
+# Controllo se è stata passata correttamente la DIR delle ISO
 if [ -z "$1" ]
 then
     echo "Utilizzo: $0 <directory>"
@@ -20,29 +20,30 @@ then
     exit 102
 fi
 
-#Controlla se lo script ha permessi di ROOT
+# Controlla se lo script ha permessi di ROOT
 if [ "$EUID" -ne 0 ]
 then
-    echo "V4V ha bisogno dei permessi di ROOT"
+    echo "V4Vendemmia ha bisogno dei permessi di ROOT"
     exit 103
 fi
 
-#Disattivo messaggi bash
+# Disattivo messaggi bash
 /bin/dmesg -n 1
 
-#Controllo dei hdd che non potranno essere scritti
+# Controllo dei hdd che non potranno essere scritti
 whiptail --backtitle "$TITLE" --msgbox "Assicurati di NON aver inserito la chiavetta su cui vuoi che gli utenti scrivano i sistemi operativi!" 10 70
 
-#Devices non scrivibili
+# Devices non scrivibili
 DEVICES_UNWRITABLE="$(lsblk -d -n -o NAME)"
 
-#Funzione USB
+
+# Funzione USB
 USB ()
 {
-    #Construisco una lista delle iso partendo dalla DIR
+    # Construisco una lista delle iso partendo dalla DIR
     OPTIONS=$(ls -l -h "$dirname" | egrep *.iso | awk '{print $9" "$5"B"" off"}')
 
-    #Controllo se esistono delle ISO
+    # Controllo se esistono delle ISO
     if [ -z "$OPTIONS" ]
     then
         whiptail --backtitle "$TITLE" --msgbox "Nessuna ISO trovata!\nAssicurati che la directory sia giusta." 10 50 3>&1 1>&2 2>&3
@@ -51,7 +52,7 @@ USB ()
      
     iso=
             
-    #Selezione della ISO
+    # Selezione della ISO
     while [ -z "$iso" ]        
     do      
         iso=$(whiptail --backtitle "$TITLE" --radiolist "Scegli il sistema da portarti a casa:" 25 65 20 ${OPTIONS} 3>&1 1>&2 2>&3)
@@ -68,7 +69,7 @@ USB ()
 
     DEVICES_AVAILABLE=
 
-    #Cerco il device su cui devo scrivere
+    # Cerco il device su cui devo scrivere
     while [ -z "$DEVICES_AVAILABLE" ]
     do
         DEVICES_AVAILABLE="$(lsblk -d -n -o NAME)";
@@ -95,36 +96,37 @@ USB ()
         fi
     done
 
-    #Scrivo su device
     whiptail --backtitle "$TITLE" --yesno "Stai per scrivere l'immagine $iso sul device $DEVICE. Sei sicuro?" 12 45 3>&1 1>&2 2>&3
     if [ $? == "0" ]
     then
+        # Scrivo su device
         (pv -n "${dirname}"/"${iso}" | /bin/dd of="${DEVICE}") 2>&1 | whiptail --backtitle "$TITLE" --gauge "Please wait..." 7 100 0
         espeak -v it "Fatto tutto. Goditi la tua $iso!" 2> /dev/null &
         whiptail --backtitle "$TITLE" --msgbox "Fatto tutto. Goditi la tua $iso!" 10 40
+
+        # Salvo nel log
         echo "$HOSTNAME" "$iso" $(date '+%A %W %Y %X') >> "v4vendemmia.log"
     else
         espeak -v it "Paura eh?! Ricorda: meglio un giorno da leoni..." 2>/dev/null &
         whiptail --backtitle "$TITLE" --msgbox "Paura eh?! Ricorda: meglio un giorno da leoni..." 10 40
     fi
 }
-###################################
+
 
 DVD ()
 {
-    #Selezione ISO
+    # Selezione ISO
 
 	eject
 	DEVICE="/media/cdrom"
-    #Scrivo ISO
+    # Scrivo ISO
 }
 
-##################################
 
-#Main
+# Main
 while [ 1 ]
 do
-	#Imposto Contatore	
+	# Imposto Contatore	
     cont=$(cat "v4vendemmia.log" | wc -l)
 	
 	choice=$(whiptail --title "Menu Principale" --backtitle "$TITLE" --cancel-button "Esci" --menu "Scegli:" 20 78 8 \
